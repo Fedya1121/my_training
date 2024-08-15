fa49f717ae2583bc84ebd260beffd0c13a58ce51
Fedya1121 <fiedia_bielkin@mail.ru>
2024-08-15 22:47:05 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Sasha': 2002,'Fedya': 2001,'Artem': 1999}
+print(my_dict)
+print(my_dict['Fedya'])
+my_dict.update({'Gleb':1989,'Nikita':1995})
+print(my_dict)
+del my_dict['Sasha']
+print(my_dict)
+my_set = {1,1,1,2,2,3,3}
+print(my_set)
+print(my_set.add(5))
+print(my_set.add('string'))
+print(my_set)
+print(my_set.discard(2))
+print(my_set)
+
