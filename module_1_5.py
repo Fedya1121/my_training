5e251082e27ec4026680e733d482da27051a4d06
Fedya1121 <fiedia_bielkin@mail.ru>
2024-08-15 23:37:52 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,12 @@
+immutable_war = 1, 2, True, 'Fedya'
+print(immutable_war)
+
+#mutable_list = ['fox','dog','cat','mouse']
+#print(mutable_list)
+#mutable_list[0] = 23
+#print(mutable_list)
+#mutable_list[3] = 'pen'
+#mutable_list.append(True)
+#print(mutable_list)
+#mutable_list.extend(['banana',2])
+#print(mutable_list)
