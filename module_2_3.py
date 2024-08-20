135f7614d035ffa621a6971e221d5a7de862037c
Fedya1121 <fiedia_bielkin@mail.ru>
2024-08-20 22:03:42 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,45 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    element = my_list[index]
+    index += 1
+    if element > 0:
+        print(element)
+        continue
+    elif element == 0:
+        continue
+    elif element < 0:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
