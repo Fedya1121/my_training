84acde71e3fb8428196382a79d554f8ea2b40d1e
Fedya1121 <fiedia_bielkin@mail.ru>
2024-10-13 14:31:59 +0500

    Add files via upload

--- module_7_5.py
+++ module_7_5.py
@@ -3,12 +3,11 @@ import time
 
 
 for root, dirs, files in os.walk(r'C:\Users\Саша-Федя\PycharmProjects\pythonProject4\.venv\Scripts\madule_7_5.py'):
-  for file in files:
-    filepath = os.path.join(root, name)
-    filetime = os.path.getctime(file_path)
-    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-    filesize = os.path.getsize(file_path)
-    parent_dir = os.path.dirname('p')
-    print(f'Обнаружен файл: {file}, Путь: {filepath},'
-          f' Размер: {filesize} байт, Время изменения: {formatted_time},'
-          f' Родительская директория: {parent_dir}')
+    for name in files:
+        filepath = os.path.join(root, name)
+        filetime = os.path.getctime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname('.venv')
+        print(f'Обнаружен файл: {name}, Путь: {filepath}, Размер: {filesize} байт, Время изменения:'
+              f' {formatted_time}, Родительская директория: {parent_dir}')
